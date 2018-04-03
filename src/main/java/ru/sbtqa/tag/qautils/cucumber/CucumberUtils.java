0aaaff89cbd4c14fc366b40042f72d03ee1be6c0
Konstantin Maltsev <mkypers@gmail.com>
2018-04-03 13:44:18 +0700

    check fail on scenario language get
    add tests

--- src/main/java/ru/sbtqa/tag/qautils/cucumber/CucumberUtils.java
+++ src/main/java/ru/sbtqa/tag/qautils/cucumber/CucumberUtils.java
@@ -6,16 +6,29 @@ import cucumber.runtime.model.CucumberFeature;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CucumberUtils {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CucumberUtils.class);
+    private static final String DEFAULT_LANGUAGE = "en";
+
+    private CucumberUtils() {
+    }
 
     public static Locale getLocale(Scenario scenario) {
         List<String> scenarioPaths = new ArrayList<>();
         scenarioPaths.add(scenario.getUri());
 
         MultiLoader multiLoader = new MultiLoader(ClassLoader.getSystemClassLoader());
-        CucumberFeature load = CucumberFeature.load(multiLoader, scenarioPaths).get(0);
-        String language = load.getGherkinFeature().getFeature().getLanguage();
+        String language = DEFAULT_LANGUAGE;
+        try {
+            CucumberFeature cucumberFeature = CucumberFeature.load(multiLoader, scenarioPaths).get(0);
+            language = cucumberFeature.getGherkinFeature().getFeature().getLanguage();
+        } catch (Exception e) {
+            LOG.warn("Error while reading feature with uri {}. Using default language {} as fallback", scenario.getUri(), DEFAULT_LANGUAGE, e);
+        }
 
         return new Locale(language);
     }
---
+++ src/test/java/ru/sbtqa/tag/qautils/cucumber/CucumberUtilsTest.java
@@ -0,0 +1,34 @@
+package ru.sbtqa.tag.qautils.cucumber;
+
+import cucumber.api.Scenario;
+import cucumber.runtime.ScenarioImpl;
+import java.util.Locale;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CucumberUtilsTest {
+
+    @Test
+    public void returnCorrectLanguageLocaleTest() {
+        Scenario scenario = mock(ScenarioImpl.class);
+        when(scenario.getUri()).thenReturn("src/test/resources/features/Correct.feature");
+        Locale expectedLocale = new Locale("ru");
+
+        Locale actualLocale = CucumberUtils.getLocale(scenario);
+
+        Assert.assertEquals(expectedLocale, actualLocale);
+    }
+
+    @Test
+    public void fallbackToDefaultLanguageTest() {
+        Scenario scenario = mock(ScenarioImpl.class);
+        when(scenario.getUri()).thenReturn("src/test/resources/features/Empty.feature");
+        Locale expectedLocale = new Locale("en");
+
+        Locale actualLocale = CucumberUtils.getLocale(scenario);
+
+        Assert.assertEquals(expectedLocale, actualLocale);
+    }
+}
